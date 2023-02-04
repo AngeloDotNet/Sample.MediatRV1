bd1e1ceb565ffb34bdf1606c144e1781a1ad5542
Angelo Pirola <angelo9871@gmail.com>
2023-02-04 21:04:23 +0100

    Add method MediatR GET

---
+++ src/Sample.API/MediatR/GetPeopleListQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Sample.API.Entity;
+
+namespace Sample.API.MediatR;
+
+public class GetPeopleListQuery : IRequest<List<PersonEntity>>
+{
+}
---
+++ src/Sample.API/MediatR/GetPersonQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Sample.API.Entity;
+
+namespace Sample.API.MediatR;
+
+public class GetPersonQuery : IRequest<PersonEntity>
+{
+    public Guid Id { get; set; }
+
+    public GetPersonQuery(Guid id)
+    {
+        Id = id;
+    }
+}
